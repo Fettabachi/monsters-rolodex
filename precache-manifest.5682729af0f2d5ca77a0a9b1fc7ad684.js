17777cdcce85802ec59fe984c94d50a308567f77
Tim Fetter <tim.fetter@magicjack.com>
2020-03-27 13:23:02 -0400

    Updates

---
+++ precache-manifest.5682729af0f2d5ca77a0a9b1fc7ad684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b9465794fae47b05cbfe5e19deec8c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4b8c2d25db28bbcc7583",
+    "url": "/monsters-rolodex/static/css/main.6e9cc799.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8c2d25db28bbcc7583",
+    "url": "/monsters-rolodex/static/js/main.e4e7cf54.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
